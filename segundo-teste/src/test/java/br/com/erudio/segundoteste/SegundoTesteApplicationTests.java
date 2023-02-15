d809f2953bf09d15c3ec399a86d5c79aa84a2fcc
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-02-15 12:15:07 -0300

    Criando meu segundo projeto com SpringBoot

---
+++ segundo-teste/src/test/java/br/com/erudio/segundoteste/SegundoTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erudio.segundoteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundoTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
